03dd121e33b366cb01d958c318ecb22b27fc13c6
saram <saram.chaudharyaman2408@gmail.com>
2023-04-20 23:29:00 +0530

    work: completed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('dashboard', req.userinfo)
+    res.render('dashboard', req.userContext?.userinfo)
 })
 
 module.exports = router
